0561d480e03bec6ea473fd04a411ca163796cd24
codelab-coder <nandaomguimaraes@gmail.com>
2025-01-10 05:55:32 -0300

    Add files via upload

---
+++ exercício com for.py
@@ -0,0 +1,4 @@
+for numero in range(1, 11):
+    print(numero)
+    if numero==10:
+        break
---
+++ for exemplo 1.py
@@ -0,0 +1,11 @@
+letras=["A","B","c","D","E"]
+
+for i in letras:
+    print("posicao:",i)
+
+print("\n")
+
+for i , letras in enumerate(letras):
+    print(i, letras)
+for i in "python":
+    print(i)
